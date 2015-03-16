3721339352b297bd90dbe00bbd9c62bb098ecd58
Phil Plückthun <phil@plckthn.me>
2015-03-16 16:40:04 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 chown www-data:www-data /app -R
+rm -f /var/log/apache2/access.log
 source /etc/apache2/envvars
-exec apache2 -D FOREGROUND
+apache2
+exec tail -f /var/log/apache2/access.log
